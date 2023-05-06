67b32082a4899724dcab4823b0372bbc6516aba8
vijay18399 <vijayreddy18399@gmail.com>
2023-05-06 14:41:35 +0530

    cors added

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 require("dotenv/config");
 const movieRoutes = require('./routes/movie.routes');
 const authRoutes = require('./routes/auth.routes');
-const app = express();
-
+const cors = require('cors');  
+const app = express();   
+app.use(cors()); 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
